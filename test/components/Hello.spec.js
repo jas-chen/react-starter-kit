e87e0ad17ae180cc176d3377b421fa24834a874d
Jas Chen <jas228@gmail.com>
2016-04-19 18:00:58 +0800

    Add test

---
+++ test/components/Hello.spec.js
@@ -0,0 +1,17 @@
+import expect from 'expect';
+import React from 'react';
+import { shallow } from 'enzyme';
+import Hello from '../../src/components/Hello/Hello';
+
+function setup(name) {
+  return shallow(
+    <Hello name={name} />
+  );
+}
+
+describe('Hello component', () => {
+  it('should display name', () => {
+    const component = setup('Jas Chen')
+    expect(component.find('div').text()).toMatch(/^Hello Jas Chen$/);
+  })
+})
