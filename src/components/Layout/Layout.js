eb392cc79ebeaa69859368bb3ae671c3e57bec02
Jas Chen <jas228@gmail.com>
2016-04-20 14:05:00 +0800

    add react-css-modules

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import { Link } from 'react-router';
+import rcm from 'react-css-modules';
+const styles = require('./Layout.scss');
 
 function Layout(props) {
-  const style = require('./Layout.scss');
-
   return (
     <div>
-      <div className={ style.logo }>React Starter Kit</div>
+      <div styleName="logo">React Starter Kit</div>
       <ul>
         <li><Link to="/about">About</Link></li>
         <li><Link to="/repos">Repos</Link></li>
@@ -20,4 +20,4 @@ Layout.propTypes = {
   children: React.PropTypes.element.isRequired,
 };
 
-export default Layout;
+export default rcm(Layout, styles);
